ad492c6ac4d60842a5689e353316af8fb8677e62
Brad Efting <bradefting@gmail.com>
2017-04-07 16:55:01 -0600

    content in migration and seed files

--- migrations/20170407155617_portfolio_projects.js
+++ migrations/20170407155617_portfolio_projects.js
@@ -1,8 +1,19 @@
+"use strict";
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('portfolio_projects', function(table){
+    table.increments();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.string('technologies').notNullable();
+    table.string('image').notNullable();
+    table.string('deployed_site').notNullable();
+    table.string('github_link').notNullable();
+    table.string('video_link').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('portfolio_projects');
 };
--- seeds/1_portfolio_projects.js
+++ seeds/1_portfolio_projects.js
@@ -1,13 +1,54 @@
+"use strict";
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('portfolio_projects').del()
     .then(function () {
+      return Promise.all([
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+        knex('portfolio_projects').insert({
+          id: 1,
+          title: 'TheraBot',
+          description: 'Web application that uses a bot to pre-qualify users for a spinal cord injury rehabilitation program. Uses Microsoft Bot Framework to accept and qualify users on website and on Facebook Messenger using same code.',
+          technologies: 'Microsoft Bot Framework, Azure App Services, NodeJS, Bootstrap',
+          image: 'n',
+          deployed_site: 'http://therabot.azurewebsites.net/',
+          github_link: 'https://github.com/bradefting',
+          video_link: 'https://www.youtube.com/watch?v=vWmQPOAuPQs'
+        }),
+        knex('portfolio_projects').insert({
+          id: 2,
+          title: 'GooseHire',
+          description: "Web application that matches a user's skills with job descriptions using the Indeed job search API. Users can save job searches using MongoDB.",
+          technologies: 'AngularJS, NodeJS, MongoDB, Mongoose, Passport, Heroku',
+          image: 'n',
+          deployed_site: 'https://q3-goosehire.herokuapp.com/',
+          github_link: 'https://github.com/bradefting/q3-goosehire',
+          video_link: 'https://www.youtube.com/watch?v=vWmQPOAuPQs'
+        }),
+        knex('portfolio_projects').insert({
+          id: 3,
+          title: 'NightLite',
+          description: "Web application to search and share the best locations for stargazing. Uses PostgreSQL to store locations and ESRI's light pollution map to display.",
+          technologies: 'Materialize, jQuery, NodeJS, Express, Knex.js, PostgreSQL, Heroku',
+          image: 'n',
+          deployed_site: 'https://bradefting-nightlite.herokuapp.com/',
+          github_link: 'https://github.com/bradefting/nightlite',
+          video_link: 'https://www.youtube.com/watch?v=jhVOPEd9WoQ'
+        }),
+        knex('portfolio_projects').insert({
+          id: 4,
+          title: 'Brewzinga',
+          description: "Web application that recommends beers based on an event or the user's local weather using Weather Underground API and BreweryDB API. Single- page app also displays local breweries using AJAX and jQuery.",
+          technologies: 'Materialize, jQuery, Surge, BreweryDB API, Weather Underground API',
+          image: 'n',
+          deployed_site: 'http://brewzinga.surge.sh/',
+          github_link: 'https://github.com/bradefting/Brewzinga',
+          video_link: 'https://www.youtube.com/watch?v=aJO_bQ4lRUc&t=1s'
+        })
+      ])
+      .then(function(){
+        return knex.raw("SELECT setval('portfolio_projects_id_seq', (SELECT MAX(id) FROM portfolio_projects))");
+      });
     });
 };
