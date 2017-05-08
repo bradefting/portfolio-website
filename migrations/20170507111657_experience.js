e883898f06e617da2fd0fe52a14b21d135f820cb
Brad Efting <bradefting@gmail.com>
2017-05-08 12:47:21 -0600

    adjust migration

--- migrations/20170507111657_experience.js
+++ migrations/20170507111657_experience.js
@@ -5,7 +5,7 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('title').notNullable();
     table.string('short_description').notNullable();
-    table.string('full_description' 600).notNullable();
+    table.string('full_description', 600).notNullable();
     table.string('main_image').notNullable();
     table.string('old_image1').notNullable();
     table.string('old_image2').notNullable();
