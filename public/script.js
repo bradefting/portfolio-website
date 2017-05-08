d05b0103357d29aaab415961434b04d16568f713
Brad Efting <bradefting@gmail.com>
2017-05-07 19:40:21 -0600

    add scrollreveal

--- public/script.js
+++ public/script.js
@@ -20,9 +20,9 @@ sr.reveal('.projects-head', {
   duration: 2000,
   origin:'bottom',
 });
-sr.reveal('.projects-left', {
+sr.reveal('.projects', {
   duration: 2000,
-  origin:'left',
+  origin:'bottom',
   distance:'200px',
   delay: 1000,
 });
@@ -43,12 +43,12 @@ sr.reveal('.projects-left', {
 //   distance:'200px',
 //   delay: 1000,
 // });
-sr.reveal('.projects-right', {
-  duration: 2000,
-  origin:'right',
-  distance:'200px',
-  delay: 1000,
-});
+// sr.reveal('.projects-right', {
+//   duration: 2000,
+//   origin:'right',
+//   distance:'200px',
+//   delay: 1000,
+// });
 sr.reveal('.skills-head', {
   duration: 2000,
   origin:'bottom'
@@ -58,23 +58,27 @@ sr.reveal('.skills-box', {
   origin:'bottom',
   viewFactor: 0.25
 });
-sr.reveal('.ex-center', {
+sr.reveal('.experience-head', {
+  duration: 2000,
+  origin:'bottom',
+});
+sr.reveal('.experience', {
   duration: 2000,
   origin:'bottom',
   viewFactor: 0.2
 });
-sr.reveal('.ex-left', {
-  duration: 3000,
-  origin:'left',
-  distance:'300px',
-  viewFactor: 0.25
-});
-sr.reveal('.ex-right', {
-  duration: 3000,
-  origin:'right',
-  distance:'300px',
-  viewFactor: 0.25
-});
+// sr.reveal('.ex-left', {
+//   duration: 3000,
+//   origin:'left',
+//   distance:'300px',
+//   viewFactor: 0.25
+// });
+// sr.reveal('.ex-right', {
+//   duration: 3000,
+//   origin:'right',
+//   distance:'300px',
+//   viewFactor: 0.25
+// });
 sr.reveal('.education-head', {
   duration: 2000,
   origin:'bottom',
